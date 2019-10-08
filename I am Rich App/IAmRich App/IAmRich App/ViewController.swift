c3337132a9d9fd6149cb99689a793312c94075ea
aijaz <aijazmike90@gmail.com>
2019-10-08 13:25:05 +0500

    I am Rich App backup

---
+++ I am Rich App/IAmRich App/IAmRich App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IAmRich App
+//
+//  Created by mac on 10/8/19.
+//  Copyright © 2019 Fazal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
